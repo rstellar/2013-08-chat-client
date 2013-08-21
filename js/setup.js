31ea75b10c6fc21a02185cf596b1c0dddb2bfbbd
Hack Reactor <hackreactor@Hacks-Mac-mini-2.local>
2013-08-20 17:35:17 -0700

    Befriends: user, bold their messages

--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,4 @@
-window.userName = window.location.search.split('=')[1];
+
 if(!/(&|\?)username=/.test(window.location.search)){
   var newSearch = window.location.search;
   if(newSearch !== '' & newSearch !== '?'){
@@ -15,30 +15,40 @@ $.ajaxPrefilter(function(settings, _, jqXHR) {
   jqXHR.setRequestHeader("X-Parse-REST-API-Key", "QC2F43aSAghM97XidJw8Qiy1NXlpL5LR45rhAVAf");
 });
 
+var userName = window.location.search.split('=')[1];
+var myFriends = {};
+
+
 
 $(document).ready(function() {
 
   $('.button').on('click',function(){
-    var message = $('.draft').val();
-    var newMessage = makeMessage(window.userName, message);
-    console.log('{"userName":'+ newMessage.userName +
-          ',"messageText":'+newMessage.messageText+'}');
+    var messageToSend = $('.draft').val();
+
+    var newMessage = makeMessage(userName, messageToSend);
+
+
     $.ajax({
       type: "POST",
       url: "https://api.parse.com/1/classes/messages",
+      contentType: 'application/json',
       data: JSON.stringify(newMessage),
-      success: function(payload){
-        // console.log(payload);
-      }
 
+      success: function(results){
+        console.log("success");
+      },
+
+      error: function(error){
+        alert(error.code);
+      }
     });
   });
 
 
   var makeMessage = function(userName, messageText) {
     var message = {};
-    message.userName = userName;
-    message.messageText = messageText.toString();
+    message.username = userName;
+    message.text = messageText.toString();
     return message;
   };
 
@@ -46,20 +56,38 @@ $(document).ready(function() {
   lastDate.setHours(lastDate.getHours()-1);
   lastDate = lastDate.toISOString();
 
+//jQuery expression that on click of a user name stores it to an object.
+$('#chats')
+  .delegate('.status','click',function(){
+    if( myFriends[ $(this).attr('id') ] ){
+      delete myFriends[$(this).attr('id')]; 
+    } else {
+      myFriends[$(this).attr('id')] = true;
+    }
+  console.log(myFriends); 
+});
 
 
 
-  var serverQuery = function() {
+
+  var fetch = function() {
     $.ajax({
       type: 'GET',
       url: 'https://api.parse.com/1/classes/messages?order=-createdAt',
       success: function(data) {
         lastDate2 = lastDate;
-        _.each(data.results, function(item){
+        _.each(data.results.reverse(), function(item){
           if (item.createdAt > lastDate){
-            var message = $('<div>').text(item.username + ": " + item.text + " at " + item.createdAt);
+            var message = $('<div id=' + item.username + '>')
+                .addClass('status')
+                .text(item.username + ": " + item.text + " at " + item.createdAt);
             $('#chats').prepend(message);
           }
+
+          if(myFriends[item.username]){
+            $('#' + item.username).addClass("friend");
+          }
+
           lastDate2 = lastDate2 > item.createdAt ? lastDate2 : item.createdAt;
         });
         lastDate = lastDate2;
@@ -67,5 +95,5 @@ $(document).ready(function() {
     });
   };
 
-  setInterval(serverQuery, 1000);
+  setInterval(fetch, 1000);
 });
